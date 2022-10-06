6f4cbdf413e3fa028dee6fb17e21d6d9e620e118
maximilian.mauroner <maximilian@mauroner.eu>
2022-10-06 16:14:32 +0200

    done k3 programming example

--- Einstufungstest/src/Testaufgabe.java
+++ Einstufungstest/src/Testaufgabe.java
@@ -1,32 +1,37 @@
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+
 public class Testaufgabe {
     public static void main(String[] args) {
-        short result;
-        String test = "Teststring_Einstufungstest";
-        System.out.println(myMulti(21, 4));
-        System.out.println(myMulti(2, 3500));
-        result = (short) myMulti(3, 7);
-        System.out.println(getThird("toss", "a", "coin"));
-        System.out.println(getThird("Teststring", "bleibt", test.substring(0, 10)));
-        System.out.println(getThird("EP1", "EP1", "EP1"));
-
-        System.out.println(replaceNthChar(test, 1, '0'));
-        System.out.println(replaceNthChar(test, 3, '#'));
-        System.out.println(replaceNthChar(test, 10, '-'));
-        System.out.println(replaceNthChar(test, 30, 'X'));
-        System.out.println(replaceNthChar("A", 1, '#'));
-        printPattern(3, '*');
-        printPattern(5, '+');
-
-        int[][] data0 = {{3, 0}, {0, 1}, {2, 2}};
-        int[][] data1 = {{0, 1, 0, 0, 1, 0}, {}, {2, 2, 2, 2, 0, 1}};
-        int[] target1 = {0, 0, 0};
-        int[] target2 = {9, 9, 9, 9};
-
-        toStringify2D(labelPath(3, new int[][]{}));
-        toStringify2D(labelPath(4, data0));
-        findMatches(data0, data0[1], target1);
-        findMatches(data1, data0[1], target1);
-        findMatches(data1, data0[2], target2);
+//        short result;
+//        String test = "Teststring_Einstufungstest";
+//        System.out.println(myMulti(21, 4));
+//        System.out.println(myMulti(2, 3500));
+//        result = (short) myMulti(3, 7);
+//        System.out.println(getThird("toss", "a", "coin"));
+//        System.out.println(getThird("Teststring", "bleibt", test.substring(0, 10)));
+//        System.out.println(getThird("EP1", "EP1", "EP1"));
+//
+//        System.out.println(replaceNthChar(test, 1, '0'));
+//        System.out.println(replaceNthChar(test, 3, '#'));
+//        System.out.println(replaceNthChar(test, 10, '-'));
+//        System.out.println(replaceNthChar(test, 30, 'X'));
+//        System.out.println(replaceNthChar("A", 1, '#'));
+//        printPattern(3, '*');
+//        printPattern(5, '+');
+//
+//        int[][] data0 = {{3, 0}, {0, 1}, {2, 2}};
+//        int[][] data1 = {{0, 1, 0, 0, 1, 0}, {}, {2, 2, 2, 2, 0, 1}};
+//        int[] target1 = {0, 0, 0};
+//        int[] target2 = {9, 9, 9, 9};
+//
+//        toStringify2D(labelPath(3, new int[][]{}));
+//        toStringify2D(labelPath(4, data0));
+//        findMatches(data0, data0[1], target1);
+//        findMatches(data1, data0[1], target1);
+//        findMatches(data1, data0[2], target2);
+        System.out.println(insertMiddle("XY", "abc")); //0-12/34-56.7-89/01-23
+        System.out.println(insertMiddle("01234", "abc")); //0-12/34-56.7-89/01-23
+        System.out.println(insertMiddle("01234567890123", "./-")); //0-12/34-56.7-89/01-23
     }
 
     public static void toStringify2D(int[][] temp) {
@@ -133,9 +138,19 @@ public class Testaufgabe {
         return counter;
     }
 
-    public static String insertMiddle(String input,String seps){
-        String ret = "";
-
-        return ret;
+    public static String insertMiddle(String input, String seps) {
+        if (input.length() <= 1 || seps.length() == 0) {
+            return input;
+        }
+        int half = input.length() / 2;
+        String leftHalf = input.substring(0, half);
+        String rightHalf = input.substring(half);
+        String remaining;
+        if (seps.length() == 1) {
+            remaining = "";
+        } else {
+            remaining = seps.substring(1);
+        }
+        return insertMiddle(leftHalf, remaining) + seps.charAt(0) + insertMiddle(rightHalf, remaining);
     }
 }
