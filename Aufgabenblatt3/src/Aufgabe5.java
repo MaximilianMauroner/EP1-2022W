51f806a767d663c9691db7e05e6d3996e98dfec1
maximilian.mauroner <maximilian@mauroner.eu>
2022-11-24 21:09:09 +0100

    done with aufgabe 5, both parts, now comments are needed

--- Aufgabenblatt3/src/Aufgabe5.java
+++ Aufgabenblatt3/src/Aufgabe5.java
@@ -7,11 +7,8 @@ import codedraw.CodeDraw;
 import java.awt.*;
 
 public class Aufgabe5 {
-    public static int c = 0;
-
     private static void drawCirclePatternRecursively(CodeDraw myDrawObj, int x, int y, int r) {
         if (r > 2) {
-            c++;
             drawCirclePatternRecursively(myDrawObj, x - r, y - r, r / 2);
             drawCirclePatternRecursively(myDrawObj, x - r, y + r, r / 2);
             drawCirclePatternRecursively(myDrawObj, x + r, y - r, r / 2);
@@ -24,10 +21,40 @@ public class Aufgabe5 {
     }
 
     private static void drawCirclePatternIteratively(CodeDraw myDrawObj, int maxRadius) {
-        int r = 2;
-        int counter = (int) (Math.log(maxRadius) / Math.log(r));
-        for (int i = 0; i < 10; i++) {
-            System.out.println(Math.pow(4, i));
+//        rad 128 = 1365    +1024
+//        rad 64 = 341  +256
+//        rad 32 = 85   +64
+//        rad 16 = 21   +16
+//        rad 8 = 5     +4
+//        rad 4 = 1     +1
+//        rad 2 = 0;
+
+        int r = 4;
+        int counter = (int) (Math.log(maxRadius) / Math.log(2)) - 2;
+
+        int x = r * 2;
+        int y = r * 2;
+        int temp = 0;
+
+        for (int i = 0; counter >= 0; i++) {
+            myDrawObj.setColor(Color.orange);
+            myDrawObj.fillCircle(x, y, r);
+            myDrawObj.setColor(Color.red);
+            myDrawObj.drawCircle(x, y, r);
+            if (i - temp == Math.pow(4, counter)) {
+                temp += Math.pow(4, counter);
+                counter--;
+                r *= 2;
+                x = r * 2;
+                y = r * 2;
+                continue;
+            }
+            if (x + r * 4 >= myDrawObj.getWidth()) {
+                x = r * 2;
+                y += r * 4;
+            } else {
+                x += r * 4;
+            }
         }
     }
 
@@ -36,11 +63,11 @@ public class Aufgabe5 {
         int width = 512;
         int height = 512;
         CodeDraw myDrawObjR = new CodeDraw(width, height);
+        CodeDraw myDrawObj = new CodeDraw(width, height);
         drawCirclePatternRecursively(myDrawObjR, width / 2, height / 2, rad);
-//        drawCirclePatternIteratively(myDrawObjR, rad);
+        drawCirclePatternIteratively(myDrawObj, rad);
         myDrawObjR.show();
-//        System.out.println(c);
-
+        myDrawObj.show();
     }
 }
 
