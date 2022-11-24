7a942d142facaae3cbc2363bb8c1c807995faa31
maximilian.mauroner <maximilian@mauroner.eu>
2022-11-24 21:15:29 +0100

    comments added for better understanding

--- Aufgabenblatt3/src/Aufgabe5.java
+++ Aufgabenblatt3/src/Aufgabe5.java
@@ -11,7 +11,7 @@ public class Aufgabe5 {
         if (r > 2) {
             drawCirclePatternRecursively(myDrawObj, x - r, y - r, r / 2);
             drawCirclePatternRecursively(myDrawObj, x - r, y + r, r / 2);
-            drawCirclePatternRecursively(myDrawObj, x + r, y - r, r / 2);
+            drawCirclePatternRecursively(myDrawObj, x + r, y - r, r / 2); // this line needs to be removed for picture b
             drawCirclePatternRecursively(myDrawObj, x + r, y + r, r / 2);
             myDrawObj.setColor(Color.orange);
             myDrawObj.fillCircle(x, y, r);
@@ -20,30 +20,38 @@ public class Aufgabe5 {
         }
     }
 
+    /**
+     * Increasing amounts
+     * rad 128 = 1365    +1024
+     * rad 64 = 341  +256
+     * rad 32 = 85   +64
+     * rad 16 = 21   +16
+     * rad 8 = 5     +4
+     * rad 4 = 1     +1
+     * rad 2 = 0;
+     *
+     * @param myDrawObj
+     * @param maxRadius
+     */
     private static void drawCirclePatternIteratively(CodeDraw myDrawObj, int maxRadius) {
-//        rad 128 = 1365    +1024
-//        rad 64 = 341  +256
-//        rad 32 = 85   +64
-//        rad 16 = 21   +16
-//        rad 8 = 5     +4
-//        rad 4 = 1     +1
-//        rad 2 = 0;
 
         int r = 4;
-        int counter = (int) (Math.log(maxRadius) / Math.log(2)) - 2;
+//        calculate the amount of radius duplications
+        int increments = (int) (Math.log(maxRadius) / Math.log(2)) - 2;
 
         int x = r * 2;
         int y = r * 2;
-        int temp = 0;
+        int counter = 0;
 
-        for (int i = 0; counter >= 0; i++) {
+        for (int i = 0; increments >= 0; i++) {
             myDrawObj.setColor(Color.orange);
             myDrawObj.fillCircle(x, y, r);
             myDrawObj.setColor(Color.red);
             myDrawObj.drawCircle(x, y, r);
-            if (i - temp == Math.pow(4, counter)) {
-                temp += Math.pow(4, counter);
-                counter--;
+//            from increments count down so that for each power of 4 the radius changes, keeping track of the sum
+            if (i - counter == Math.pow(4, increments)) {
+                counter += Math.pow(4, increments);
+                increments--;
                 r *= 2;
                 x = r * 2;
                 y = r * 2;
@@ -62,12 +70,12 @@ public class Aufgabe5 {
         int rad = 128;
         int width = 512;
         int height = 512;
-        CodeDraw myDrawObjR = new CodeDraw(width, height);
-        CodeDraw myDrawObj = new CodeDraw(width, height);
-        drawCirclePatternRecursively(myDrawObjR, width / 2, height / 2, rad);
-        drawCirclePatternIteratively(myDrawObj, rad);
-        myDrawObjR.show();
-        myDrawObj.show();
+        CodeDraw recursiveCodeDraw = new CodeDraw(width, height);
+        CodeDraw iterativeCodeDraw = new CodeDraw(width, height);
+        drawCirclePatternRecursively(recursiveCodeDraw, width / 2, height / 2, rad);
+        drawCirclePatternIteratively(iterativeCodeDraw, rad);
+        recursiveCodeDraw.show();
+        iterativeCodeDraw.show();
     }
 }
 
