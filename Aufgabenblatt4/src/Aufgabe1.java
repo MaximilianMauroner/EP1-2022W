cdf6cc4a684b17847eeec9190cf6b00c26d95c69
maximilian.mauroner <maximilian@mauroner.eu>
2022-12-03 16:37:38 +0100

    aufgabe 1 and 2 ab done

--- Aufgabenblatt4/src/Aufgabe1.java
+++ Aufgabenblatt4/src/Aufgabe1.java
@@ -33,7 +33,7 @@ public class Aufgabe1 {
     }
 
     private static void printArray(int[] workArray) {
-        for (int i = 0; i < workArray.length; i--) {
+        for (int i = 0; i < workArray.length; i++) {
             System.out.print(workArray[i] + " ");
         }
         System.out.println();
@@ -54,15 +54,35 @@ public class Aufgabe1 {
         printArray(filledArray);
     }
 
-    //**************************************************************************
-    //**** Notizen und Fragebeantwortungen bitte hier unterhalb durchführen! ***
-    //**************************************************************************
+    //
+    // Notizen und Fragebeantwortungen bitte hier unterhalb durchführen! 
+    //
     //Frage 1:
-    //
+    // der Fehler ist das [-1] in keinem array existiert also kann man nicht darauf zugreifen
     //Frage 2:
-    //
+    // Da Arrays als Referenz übergeben wird und nicht als Wert (call by reference, call by value)
     //Frage 3:
-    //
+    // hier wird nicht eine Kopie erstellt, sondern nur die Referenz "übergeben", dies sieht man, auch wenn man ein sout der beiden Arrays macht
     //Frage 4:
-    //
+    // Denn hier wird nur der Variable in der Funktion die Referenz des Arrays zugewiesen und nicht, aber filledArray hat immer noch die gleiche Referenz auf das originale array
+
+//      Zusatzfrage(n): Gehen Sie hier von eindimensionalen Arrays aus!
+//
+//      1. Welchen Datentyp muss der Indexausdruck haben, mit dem die Position in einem Array bestimmt wird?
+//          Integer
+//
+//      2. Mussen Sie ein Array initialisieren?
+//          Ja
+//
+//      3. Wie kann die Länge eines Arrays verändert werden?
+//          Gar nicht, es muss eine neues array erstellt werden
+//
+//      4. Wie gehen Sie vor, wenn Sie ein int-Array kopieren mussen?
+//          Entweder in einem for loop alle werte einzeln kopieren und in einem neuen array speichern oder funktionen der Arrays Klasse benutzten
+//
+//      5. Beginnt die Indexzählung eines Arrays immer bei 0?
+//          Ja
+//
+//      6. Ist es sinnvoll, zwei Arrays mit "==" zu vergleichen? Was passiert im Detail, bei einem Vergleich mit "=="?
+//          Ja, wenn man vergleichen will, ob beide Variablen die gleiche Referenz haben.
 }
--- Aufgabenblatt4/src/Aufgabe2.java
+++ Aufgabenblatt4/src/Aufgabe2.java
@@ -5,6 +5,30 @@ public class Aufgabe2 {
 
     public static void main(String[] args) {
         // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        int[] a_aufgabe_arr = new int[]{6, 1, 8, 2, 5, 3, 4, 7, 9, 0};
+        int[] b_aufgabe_arr = new int[12];
+        for (int i = 0; i < a_aufgabe_arr.length; i++) {
+            if (a_aufgabe_arr.length == i + 1) {
+                System.out.print(a_aufgabe_arr[i]);
+            } else {
+                System.out.print(a_aufgabe_arr[i] + "#");
+            }
+        }
+//
+        System.out.println();
+//
+        for (int i = 0; i < b_aufgabe_arr.length; i++) {
+            b_aufgabe_arr[i] = (i + 1) * 6;
+            if (b_aufgabe_arr[i] % 9 == 0) {
+                b_aufgabe_arr[i] = 0;
+            }
+            if (b_aufgabe_arr.length == i + 1) {
+                System.out.print(b_aufgabe_arr[i]);
+            } else {
+                System.out.print(b_aufgabe_arr[i] + " ");
+            }
+        }
+//
     }
 }
 
